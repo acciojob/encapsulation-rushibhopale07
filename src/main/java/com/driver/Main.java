43abd3aa77da399526ab9697e4244bbcc1d77e26
Rushi <rushibhopale15@gmail.com>
2023-01-13 12:13:07 +0530

    Task complete

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -7,7 +7,7 @@ public class Main {
 //        System.out.println(rw.name);
         //Error get -> make a name in RWOnly is public or protected or remove a private modifier
 
-        rw.setName("Rushikesh");
-        System.out.println(rw.getName());
+        rw.writeOnly("Rushikesh");
+        System.out.println(rw.readOnly());
     }
 }
--- src/main/java/com/driver/RWOnly.java
+++ src/main/java/com/driver/RWOnly.java
@@ -2,11 +2,12 @@ package com.driver;
 public class RWOnly {
     private String name;
 
-    public String getName() {
+
+    public void writeOnly(String name) {
+        this.name = name;
+    }
+    public String readOnly() {
         return name;
     }
 
-    public void setName(String name) {
-        this.name = name;
-    }
 }
