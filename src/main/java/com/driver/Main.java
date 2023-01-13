29ceb3a8bf27cbdb41ca5650543a591ed6f9647e
Rushi <rushibhopale15@gmail.com>
2023-01-13 12:09:54 +0530

    Task 6 complete

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -3,8 +3,11 @@ public class Main {
     public static void main(String [] args)
     {
         RWOnly rw= new RWOnly();
-        rw.name="Rushikesh";
-        System.out.println(rw.name);
+//        rw.name="Rushikesh";
+//        System.out.println(rw.name);
         //Error get -> make a name in RWOnly is public or protected or remove a private modifier
+
+        rw.setName("Rushikesh");
+        System.out.println(rw.getName());
     }
 }
